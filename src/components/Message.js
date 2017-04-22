8434347eb76eaa3095fa5420b655210748750e15
Andrew Chan <andrewkchan@berkeley.edu>
2017-04-22 11:44:28 -0700

    more

--- src/components/Message.js
+++ src/components/Message.js
@@ -7,6 +7,7 @@ class Message extends React.Component {
     render() {
         return (
             <div>
+                
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-//import App from "./components/App";
+import App from "./components/App";
 import './styles/main.scss';
 
-// ReactDOM.render(
-//     <App />,
-//     document.getElementById("main")
-// );
+ReactDOM.render(
+    <App />,
+    document.getElementById("main")
+);
