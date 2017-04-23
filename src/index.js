1bd0b83db7a209f493d44e10b257ee583af8b713
Andrew Chan <andrewkchan@berkeley.edu>
2017-04-22 20:32:20 -0700

    tutorial deletions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
 import './styles/main.scss';
-
-ReactDOM.render(
-    <App />,
-    document.getElementById("main")
-);
